6a278d5219a6d97f43aca86594d51d8c25bafd5c
Marques M <154482248+Marqm713@users.noreply.github.com>
2024-06-25 10:59:36 -0500

    Update BMI Calc 1.0.py

--- BMI Calc 1.0.py
+++ BMI Calc 1.0.py
@@ -1,20 +1,15 @@
 from tkinter import *
 
-
-
-
 class BMICalc:
     def __init__(self):
         ## WINDOW ##
         window = Tk()
         window.title("Studio Super: BMI Calculator")
 
-
         ## LEFT-SIDE LABELS ##
         Label(window, text = "Enter your current height in inches: ").grid(row = 1, column = 1, sticky = W)
         Label(window, text = "Enter your current weight in pounds: ").grid(row = 2, column = 1, sticky = W)
         Label(window, text = "Your current BMI is: ").grid(row = 3, column = 1, sticky = W)
-
 
         ## RIGHT-SIDE ENTRY BOXES ##
         self.heightVar = IntVar()
@@ -22,20 +17,14 @@ class BMICalc:
         self.weightVar = IntVar()
         Entry(window, textvariable = self.weightVar, justify = RIGHT).grid(row = 2, column = 2)
 
-
         ## RIGHT-SIDE LABEL ##
         self.totalVar = StringVar()
         lbltotalVar = Label(window, textvariable = self.totalVar).grid(row = 3, column = 2, sticky = E)
 
-
         ## BMI BUTTON **
         btComputeBMI = Button(window, text = "Compute BMI", command = self.getBMI).grid( row = 6, column = 2, sticky = E)
 
-
-
-
         window.mainloop()
-
 
         ## BMI FORMULA ##
     def getBMI(self):
@@ -45,4 +34,5 @@ class BMICalc:
         self.totalVar.set(f"{totalVar: 10.2f}")
        
         return totalVar;
+        
 BMICalc()
